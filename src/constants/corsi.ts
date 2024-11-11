46831d55b6a0796baf957b2133ed7c895bb870dd
Andrei Rotaru <125825852+reyrot24@users.noreply.github.com>
2024-11-11 13:56:29 +0100

    corsi error

--- src/constants/corsi.ts
+++ src/constants/corsi.ts
@@ -11,11 +11,3 @@ export const corsi = [
   "Wing chun",
   "Spartan",
 ];
-
-`We have a weight room, a physiotherapy class, 
-a kickboxing class, 
-an aerobic gymnastics with functional method class, 
-a calisthenics class, 
-an aerobic gymnastics with total body method class, 
-an acrobatic gymnastics class, a modern and classic dance class, 
-a spartan workout class, a wing chun class and a pilates class.`;
