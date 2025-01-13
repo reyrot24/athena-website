16dca3ac68dd464d2cfa5280d18002bfb0d09eef
Andrei Rotaru <125825852+reyrot24@users.noreply.github.com>
2025-01-13 13:18:01 +0100

    Resolved error

--- src/app/api/[id]/route.ts
+++ src/app/api/[id]/route.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params;
+  const { id } = await params;
 
   const match = id.match(/^file-(.*?)-pdf$/);
   const exID = match![1];
