c64a779385b8c20f8ffa009ad6299d9c327a69ad
Andrei Rotaru <125825852+reyrot24@users.noreply.github.com>
2025-09-09 11:43:05 +0200

    ORARI in manutenzione

---
+++ src/app/(pages)/orari/page.tsx
@@ -0,0 +1,18 @@
+const Orari = async () => {
+  /* const orari: oraritypes[] = await sanityFetch({
+    query: QueryCorsiEOrari,
+    revalidate: 10,
+  }); */
+
+  return (
+    <section className="px-[5%] py-12 md:py-10 mt-[80px] bg-bg text-text">
+      {/* <h1 className="flex text-accentYellow justify-center items-center mb-14 text-4xl font-bold md:mb-20 md:text-6xl">
+        ORARI CORSI
+      </h1>
+      <ClientComponentOrari response={orari} /> */}
+      PAGINA IN MANUTENZIONE
+    </section>
+  );
+};
+
+export default Orari;
--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,8 +1,8 @@
 export const Links = [
   { title: "Chi siamo", url: "/chisiamo" },
   { title: "Corsi", url: "/corsi" },
-  { title: "Orari", url: "/orari" },
-  /*   { title: "Testimonianze", url: "/testimonianze" }, */
+  /* { title: "Orari", url: "/orari" }, */
+  { title: "Testimonianze", url: "/testimonianze" },
   { title: "Galleria", url: "/galleria" },
   { title: "News", url: "/news" },
 ];
